9c0c53f246881ad882405c7aa86ce907eb56fb66
Mugunth <mugukrish@gmail.com>
2019-05-06 11:56:07 +0530

    phomatic

---
+++ PhOMat.java
@@ -0,0 +1,16 @@
+public class PhOMat{
+  public static void main(String[] a){
+    String[] word1 = {"one", "two", "three", "four", "five", "six"};
+    String[] word2 = {"is", "the", "was", "there"};
+    String[] word3 = {"yes", "nothing", "something"};
+
+    int r1 =(int) (Math.random() * word1.length);
+    int r2 =(int) (Math.random() * word2.length);
+    int r3 =(int) (Math.random() * word3.length);
+
+    System.out.println((Math.random() * word1.length));
+    System.out.println(word1[r1] +" "+ word2[r2] +" "+word3[r3]);
+
+   }
+
+}
